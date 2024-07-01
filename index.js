3c1c51c121868da6f59778013f04ffa0b06ece2c
Bethwel123 <bethwellangat654@gmail.com>
2024-07-01 23:43:48 +0300

    Adds arrayfilters in index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+  
+function fuzzyMatch(drivers, letters) {
+    const lowerLetters = letters.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowerLetters));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name)
+}
+
+
+  
